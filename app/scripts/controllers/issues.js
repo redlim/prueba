3abc03085ba29f6f1d79ffb60a279a406db550b7
wilder <wilder_olmos@ayscom.com>
2015-12-15 23:30:06 +0100

    Añadidas nuevas funcionalidades y creacion de historias

--- app/scripts/controllers/issues.js
+++ app/scripts/controllers/issues.js
@@ -12,26 +12,21 @@ angular.module('cycleItCustomerServiceApp')
     $scope.showFormDetails = false;
     // sincronización de las issues
     $scope.issues = $firebaseArray(Ref.child('issues'));
-
+    $scope.currentIssue = {};
     // display any errors
     $scope.issues.$loaded().catch(alert);
-
-    // Método para añadir alguna incidencia
-    $scope.addIssue = function(newIssue) {
-      if( newIssue ) {
-        // push a message to the end of the array
-        $scope.messages.$add(newIssue)
-          // display any errors
-          .catch(alert);
-      }
-    };
-
     //editarIncidencia:
+    $scope.newHistory ="";
+    $scope.showNewHistory = false;
+
+    $scope.changeState = function(){
+      console.log("hola");
+      $scope.showNewHistory = true;
+    };
     $scope.issueDetails = function(issue){
+      $scope.showNewHistory = false;
       $scope.showFormDetails = true;
-      $scope.detailsName = issue.issueProject;
-      $scope.detailsDescription = issue.description;
-      $scope.detailsSummary = issue.summary;
+      $scope.currentIssue = issue;
       $scope.detailsLevel = {
         availableOptions: [
           {id: '1', name: 'Bajo'},
@@ -39,9 +34,8 @@ angular.module('cycleItCustomerServiceApp')
           {id: '3', name: 'Alto'},
           {id: '4', name: 'Crítico'}
         ],
-        selectedOption: {id: issue.level}
+        selectedOption: {id:  $scope.currentIssue.level}
       };
-      $scope.detailsHistories = issue.history;
       //estado de la incidencia: abierta, resolviéndose, se necesita
       //feedback, cerrada, resuelta, duplicada o no se resuelve
       $scope.detailsState =  {
@@ -50,16 +44,24 @@ angular.module('cycleItCustomerServiceApp')
           {id: '2', name: 'Resolviéndose'},
           {id: '3', name: 'Se necesita feedback'},
           {id: '4', name: 'Cerrada'},
-          {id: '4', name: 'Resuelta'},
-          {id: '4', name: 'Duplicada'},
-          {id: '4', name: 'No se resuelve'}
+          {id: '5', name: 'Resuelta'},
+          {id: '6', name: 'Duplicada'},
+          {id: '7', name: 'No se resuelve'}
         ],
-        selectedOption: {id:issue.state}
+        selectedOption: {id: $scope.currentIssue.state}
       };
-
     };
 
-    //$scope.i
+
+    $scope.save = function(currentIssue){
+      console.log(currentIssue.histories);
+      if(currentIssue.histories == "undefined"){
+        currentIssue.histories =[];
+      }
+      //console.log($scope.newHistory);
+      currentIssue.histories.push($scope.newHistory);
+      $scope.issues.$save(currentIssue);
+    };
 
     function alert(msg) {
       $scope.err = msg;
